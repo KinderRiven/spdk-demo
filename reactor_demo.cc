b3f24acd115199fc1f12f19232066f29dc040568
KinderRiven <1339764596@qq.com>
2021-04-22 20:36:05 +0800

    push by hsk

--- reactor_demo.cc
+++ reactor_demo.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-17 15:32:04
- * @LastEditTime: 2021-04-22 20:35:18
+ * @LastEditTime: 2021-04-22 20:35:57
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /spdk-demo/reactor_demo.cc
@@ -94,6 +94,8 @@ void stop_app()
         if (i != spdk_env_get_first_core()) {
             struct spdk_event* event = spdk_event_allocate(i, stop_event, nullptr, nullptr);
             spdk_event_call(event);
+            while (!spdk_thread_is_exited(g_core_argv[i].thread)) {
+            }
             spdk_thread_destroy(g_core_argv[i].thread);
         }
     }
