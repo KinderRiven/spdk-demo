cb4d69d998d2e75311a4ed41ec36a78bc1fe5db1
KinderRiven <1339764596@qq.com>
2021-04-28 18:20:03 +0800

    push by hsk

--- bdev_demo.cc
+++ bdev_demo.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-17 15:32:04
- * @LastEditTime: 2021-04-28 18:18:52
+ * @LastEditTime: 2021-04-28 18:19:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /spdk-demo/reactor_demo.cc
@@ -135,20 +135,28 @@ void start_app(void* cb)
     if (_bdev == nullptr) {
         printf("spdk_bdev_get_by_name failed! [Malloc0]\n");
         exit(1);
+    } else {
+        _rc = spdk_bdev_open(_bdev, true, nullptr, nullptr, &_desc);
+        if (_rc) {
+            printf("spdk_bdev_open failed!\n");
+            exit(1);
+        } else {
+            printf("spdk_bdev_open [malloc][bs:%zu][align:%zu]\n", spdk_bdev_get_block_size(_bdev), spdk_bdev_get_buf_align(_bdev));
+        }
     }
 
     _bdev = spdk_bdev_get_by_name("Nvme0");
     if (_bdev == nullptr) {
         printf("spdk_bdev_get_by_name failed! [Nvme0]\n");
         exit(1);
-    }
-
-    _rc = spdk_bdev_open(_bdev, true, nullptr, nullptr, &_desc);
-    if (_rc) {
-        printf("spdk_bdev_open failed!\n");
-        exit(1);
     } else {
-        printf("spdk_bdev_open [bs:%zu][align:%zu]\n", spdk_bdev_get_block_size(_bdev), spdk_bdev_get_buf_align(_bdev));
+        _rc = spdk_bdev_open(_bdev, true, nullptr, nullptr, &_desc);
+        if (_rc) {
+            printf("spdk_bdev_open failed!\n");
+            exit(1);
+        } else {
+            printf("spdk_bdev_open [nvme][bs:%zu][align:%zu]\n", spdk_bdev_get_block_size(_bdev), spdk_bdev_get_buf_align(_bdev));
+        }
     }
 
     int i;
