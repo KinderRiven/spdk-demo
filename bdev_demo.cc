38f205772d2ced6d79c176e1a9b6d7b82f301f9c
KinderRiven <1339764596@qq.com>
2021-04-29 17:29:23 +0800

    push by hsk

--- bdev_demo.cc
+++ bdev_demo.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-17 15:32:04
- * @LastEditTime: 2021-04-29 17:26:23
+ * @LastEditTime: 2021-04-29 17:29:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /spdk-demo/reactor_demo.cc
@@ -192,8 +192,6 @@ void start_app(void* cb)
             spdk_event_call(event);
         }
     }
-
-    while (!g_app_stop) { }
 }
 
 void stop_io_event(void* arg1, void* arg2)
@@ -236,6 +234,10 @@ void stop_app()
 
     printf("bdev close!\n");
     spdk_bdev_close(g_desc);
+
+    printf("app stop!\n");
+    spdk_app_stop(g_app_rc);
+
     g_app_stop = true;
 }
 
