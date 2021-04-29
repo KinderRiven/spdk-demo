a5a6717d59e707c730b3da665569d11f4aa3dbe7
KinderRiven <1339764596@qq.com>
2021-04-29 19:48:18 +0800

    push by hsk

--- run_perf.sh
+++ run_perf.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: your name
  # @Date: 2021-04-29 19:28:23
- # @LastEditTime: 2021-04-29 19:47:48
+ # @LastEditTime: 2021-04-29 19:48:16
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /spdk-demo/run_perf.sh
@@ -9,7 +9,7 @@
 # see : https://spdk.io/doc/nvme.html
 # for latency
 PERF=/home/hanshukai/import_libs/spdk/build/examples/perf
-$PERF -q 1 -o 64 -w randread -r 'trtype:PCIe traddr:0000:86:00.0' -t 10
+$PERF -q 1 -o 256 -w randread -r 'trtype:PCIe traddr:0000:86:00.0' -t 10
 # $PERF -q 1 -o 4096 -w randread -r 'trtype:PCIe traddr:0000:86:00.0' -t 10
 # $PERF -q 1 -o 16384 -w randread -r 'trtype:PCIe traddr:0000:86:00.0' -t 10
 # $PERF -q 1 -o 32768 -w randread -r 'trtype:PCIe traddr:0000:86:00.0' -t 10
