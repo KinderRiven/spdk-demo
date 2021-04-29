ead6211f5293d107ea4b2e808b6d75c27af2de76
KinderRiven <1339764596@qq.com>
2021-04-29 16:29:50 +0800

    push by hsk

--- bdev_demo.cc
+++ bdev_demo.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-17 15:32:04
- * @LastEditTime: 2021-04-29 16:28:18
+ * @LastEditTime: 2021-04-29 16:29:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /spdk-demo/reactor_demo.cc
@@ -211,11 +211,12 @@ void stop_app()
     }
 
     if (g_num_run_thread == 0) {
-        spdk_bdev_wait_for_examine(nullptr, nullptr);
         printf("bdev close!\n");
         spdk_bdev_close(g_desc);
-        printf("spdk_app_stop! (%d)\n", g_app_rc);
-        spdk_app_stop(g_app_rc);
+        // printf("spdk_app_stop! (%d)\n", g_app_rc);
+        // spdk_app_stop(g_app_rc);
+        printf("spdk_app_fini");
+        spdk_app_fini();
     }
 }
 
@@ -238,8 +239,5 @@ int main(int argc, char** argv)
     printf("OPT [name:%s][file_name:%s][reactor_mask:%s][main_core:%d]\n",
         _app_opts.name, _app_opts.json_config_file, _app_opts.reactor_mask, _app_opts.main_core);
     g_app_rc = spdk_app_start(&_app_opts, start_app, nullptr);
-
-    printf("spdk_app_fini");
-    spdk_app_fini();
     return 0;
 }
